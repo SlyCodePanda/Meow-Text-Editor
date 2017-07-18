1d909004b0a8d1cb2d9667c7f59e5d2dcef124d0
Renee <reneemarsland@hotmail.com>
2017-07-18 22:24:19 +0930

    adding some functionality

--- meow.py
+++ meow.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4 import QtCore, QtGui, uic
 
 
+
 # Window inherits from the Qt widget 'MainWindow'
 class Window(QtGui.QMainWindow):
 
@@ -14,7 +15,23 @@ class Window(QtGui.QMainWindow):
     
         # Starting point: 50, 50. And 500 pixels wide, 300 pixels tall.
         self.ui.setGeometry(50, 50, 500, 300)
-        self.ui.setWindowTitle("Meow Text Editor")
+        self.ui.setWindowTitle("Meow Text")
+
+
+        # Open.
+        self.ui.actionOpen.setShortcut("Ctrl+O")
+        self.ui.actionOpen.setStatusTip('Open File')
+        self.ui.actionOpen.triggered.connect(self.file_open)
+
+        # Save.
+        self.ui.actionSave.setShortcut("Ctrl+S")
+        self.ui.actionSave.setStatusTip('Save File')
+        self.ui.actionSave.triggered.connect(self.file_save)
+
+        # Exit.
+        self.ui.actionQuit.setShortcut("Ctrl+Q")
+        self.ui.actionQuit.setStatusTip('Quit Application')
+        self.ui.actionQuit.triggered.connect(self.close_application)
 
         
         self.home()
@@ -23,6 +40,46 @@ class Window(QtGui.QMainWindow):
     def home(self):
         self.ui.show()
         #self.show()
+
+
+    def editor(self):
+        #self.textEdit = QtGui.QTextEdit()
+        #self.setCentralWidget(self.textEdit)
+        print( 'Editor has loaded!')
+
+
+    def file_open(self):
+        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+        # Opening with the intention to read ('r').
+        file = open(name, 'r')
+
+        # Need to call the editor, because it is not there by default. 
+        self.editor()
+
+        with file:
+            text = file.read()
+            self.ui.textEdit.setText(text)
+
+
+    def file_save(self):
+        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        # Intention to write the file ('w')
+        file = open(name, 'w')
+        text = self.ui.textEdit.toPlainText()
+        file.write(text)
+        file.close()
+
+
+    def close_application(self):
+        # Pop-Up Message:
+        choice = QtGui.QMessageBox.question(self, 'Quit',
+                                            "Are you sure you want to leave meow?",
+                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            print( "Closing Meow...")
+            sys.exit()
+        else:
+            pass
 
 
 # Our 'main'.
